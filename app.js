9e10ad735fb07aa1787c6cc5f7f679831d5eaa88
nbo2001 <natanaelbezerra36@gmail.com>
2024-05-25 19:22:11 -0400

    Tentado encontrar o erro

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Bem vindos ao WebAcademy!! Fazendo o deploy na google cloud');
+  res.send('Bem vindos ao WebAcademy!! Fazendo o deploy na google cloud. Quando não é uma coisa e outra');
 });
 
 app.listen(8080, () => {
